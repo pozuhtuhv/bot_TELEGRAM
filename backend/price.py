665cc58b5e026391b0ff45aabeb67f30d6ec8873
Hyeonseok-Cho <pozuhtuhv@gmail.com>
2024-06-18 16:40:15 +0900

    (update) module optimize

---
+++ backend/price.py
@@ -0,0 +1,5 @@
+def price(context):
+    # 명령어 뒤에 입력된 인수를 받아옴.
+    # args = context.args
+    msg = "불러왔따~"
+    return msg
--- bit_load.py
+++ bit_load.py
@@ -1,9 +1,10 @@
 import os
+import asyncio
 from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
-import os
 from backend.info import info  # info 함수 직접 import
+from backend.price import price  # price 함수 직접 import
 
 # .env 파일 활성화
 load_dotenv()
@@ -11,7 +12,8 @@ load_dotenv()
 UPBIT_ACCESS_KEY = os.getenv('UPBIT_ACCESS_KEY')
 UPBIT_SECRET_KEY = os.getenv('UPBIT_SECRET_KEY')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-ROOM_ID = os.getenv('ROOM_ID')
+
+price_running = False
 
 # Chat info 명령어 진행
 class TelegramBotHandler:
@@ -19,8 +21,9 @@ class TelegramBotHandler:
     @classmethod
     async def command(cls, update: Update, context: ContextTypes.DEFAULT_TYPE):
         msg = '''* command info
-         - /price : 계좌 정보
-         - /info <CODE> : 코인 가격
+         - /price ON : 가격 정보 시작
+         - /price OFF : 가격 정보 중지
+         - /info <CODE> : 코인 가격 정보
         '''
         await update.message.reply_text(msg)
 
@@ -30,12 +33,35 @@ class TelegramBotHandler:
         msg = info(context)
         await update.message.reply_text(msg)
 
+    # 가격정보 불러오기
+    @classmethod
+    async def price(cls, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        global price_running
+        args = context.args
+        if args and args[0].upper() == 'OFF':
+            price_running = False
+            await update.message.reply_text('가격 정보 중지')
+        elif args and args[0].upper() == 'ON':
+            if not price_running:
+                price_running = True
+                await update.message.reply_text('가격 정보 시작')
+                asyncio.create_task(cls.send_price_updates(update, context))
+
+    @classmethod
+    async def send_price_updates(cls, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        global price_running
+        while price_running:
+            msg = price(context.args)
+            await update.message.reply_text(msg)
+            await asyncio.sleep(5)
+
 # 명령어 인식
 def main():
     try:
         application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
         application.add_handler(CommandHandler('command', TelegramBotHandler.command))
         application.add_handler(CommandHandler('info', TelegramBotHandler.info))
+        application.add_handler(CommandHandler('price', TelegramBotHandler.price))
         application.run_polling()
     except KeyboardInterrupt:
         return True
